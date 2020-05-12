49b66ac5c0e7f11483d741e0fc54b731a985f04a
julianjg <julian-guzman@upc.edu.co>
2020-05-11 19:11:12 -0500

    error insertando en seting

--- apiionic/settings.py
+++ apiionic/settings.py
@@ -128,3 +128,21 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
